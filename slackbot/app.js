b6ac3bf76136df9712b86260338fe5eeefe34a90
jaskaransarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2023-12-04 15:06:51 +0000

    refactor: 💡 abstract out fns and add posting a reply

--- slackbot/app.js
+++ slackbot/app.js
@@ -1,6 +1,8 @@
 import App from '@slack/bolt';
 import fetch from 'node-fetch';
 
+const NANO_SECOND = 1000000000
+
 // Initializes your app with your bot token and signing secret
 const app = new App.App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -17,6 +19,95 @@ const getStatus = async (ids) => {
   return await response.json();
 }
 
+const postSuccess = async (data) => {
+  if (data === null || data.length === 0) {
+    await addEmoji("sparkles")
+    return true
+  }
+  return false
+}
+
+const removeEmoji = async (emoji, ts) => {
+  return await app.client.reactions.remove({
+    name: emoji,
+    timestamp: ts
+  })
+}
+
+const addEmoji = async (emoji, ts) => {
+  return await app.client.reactions.add({
+    name: emoji,
+    channel: "C05EG79V8HW",
+    timestamp: ts
+  })
+
+}
+
+const postFail = async (data) => {
+  const failed = data.some((pr) => pr.InvalidChecks.length ? pr.InvalidChecks.some((check) => check.Status === 1) : false)
+
+  if (failed) {
+    await addEmoji("x")
+    return true
+  }
+  return false
+}
+
+const postReaction = async (data) => {
+  const isSuccess = await postSuccess(data)
+
+  if (isSuccess) {
+    return true
+  }
+
+  const isFailed = await postFail(data)
+
+  if (isFailed) {
+    return true
+  }
+
+  return false
+}
+
+const postReply = async (message, ts) => {
+  return await app.client.chat.postMessage({
+    channel: "C05EG79V8HW",
+    text: message,
+    icon_emoji: "robot_face",
+    thread_ts: ts
+  })
+}
+
+const postPendingRecnt = async (data) => {
+  const pendingRecent = data.some((pr) => pr.InvalidChecks.length ? pr.InvalidChecks.some((check) => check.Status === 2 && check.RetryInNanoSec > 0) : false)
+
+  if (pendingRecent) {
+    await addEmoji("repeat", message.ts)
+
+    setTimeout(async () => {
+      const data = await getStatus(pendingRecent.Id)
+
+      const result = postReaction(data)
+
+      await removeEmoji("repeat", message.ts)
+
+      if (result) {
+        return true
+      }
+
+      await postReply("It looks like checks on your pr are _still_ pending even after waiting a while. A Cloud Platform team member will come and take a look.", message.ts)
+      await addEmoji("hourglass_flowing_sand", message.ts)
+      await addEmoji("warning", message.ts)
+
+    }, pendingRecent.RetryIn / NANO_SECOND + 10)
+
+    return true
+  }
+
+  return false
+}
+
+
 app.message('github.com/ministryofjustice/cloud-platform-environments/pull/', async ({ message }) => {
   console.log('msg', message)
 
@@ -28,52 +119,23 @@ app.message('github.com/ministryofjustice/cloud-platform-environments/pull/', as
 
   console.log(JSON.stringify(data))
 
-  if (data === null || data.length === 0) {
-    await app.client.reactions.add({
-      name: "sparkle",
-      channel: "C05EG79V8HW",
-      timestamp: message.ts
-    })
+  const result = await postReaction(data)
+
+  if (result) {
     return
   }
 
-  const failed = data.some((pr) => pr.InvalidChecks.length ? pr.InvalidChecks.some((check) => check.Status === 1) : false)
+  const pendingResult = await postPendingRecnt(data)
 
-  if (failed) {
-    await app.client.reactions.add({
-      name: "x",
-      channel: "C05EG79V8HW",
-      timestamp: message.ts
-    })
+  if (pendingResult) {
     return
-  }
-
-  const pendingRecent = data.some((pr) => pr.InvalidChecks.length ? pr.InvalidChecks.some((check) => check.Status === 2 && check.RetryInNanoSec > 0) : false)
-
-  if (pendingRecent) {
-    await app.client.reactions.add({
-      name: "repeat",
-      channel: "C05EG79V8HW",
-      timestamp: message.ts
-    })
-    return
-
-    // TODO add retry here
-    // setTimeout(async () => {
-    //   await getStatus(pendingRecent.Id)
-    // }, pendingRecent.RetryIn * 1000)
   }
 
   const pendingOlderThan10Mins = data.some((pr) => pr.InvalidChecks.length ? pr.InvalidChecks.some((check) => check.Status === 2 && check.RetryInNanoSec === 0) : false)
 
   if (pendingOlderThan10Mins) {
-    await app.client.reactions.add({
-      name: "hourglass_flowing_sand",
-      channel: "C05EG79V8HW",
-      timestamp: message.ts
-    })
-    return
-
+    await addEmoji("hourglass_flowing_sand", message.ts)
+    await postReply("Looks like concourse needs a kick, plesase push an empty commit to retrigger the checks `git commit --allow-empty -m 'Empty - Commit'`", message.ts)
     // TODO trigger empty commit and then check again in x mins
   }
 });
