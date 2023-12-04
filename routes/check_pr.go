76d4bae23e8976ab50aabd8308c4adbf165efc7e
jaskaransarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2023-12-04 09:04:33 +0000

    refactor: 💡 check-pr to accept multiple pr ids

--- routes/check_pr.go
+++ routes/check_pr.go
@@ -2,60 +2,63 @@ package routes
 
 import (
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/go-github/github"
-	"github.com/ministryofjustice/cloud-platform-hammer-bot/utils"
-
 	"github.com/ministryofjustice/cloud-platform-hammer-bot/pull_requests"
+	"github.com/ministryofjustice/cloud-platform-hammer-bot/utils"
 )
 
 func InitGetCheckPR(r *gin.Engine, ghClient *github.Client) {
-	r.GET("/check-pr/:pr-number", func(c *gin.Context) {
-		prNumber := c.Param("pr-number")
+	r.GET("/check-pr", func(c *gin.Context) {
+		ids := c.Query("id")
+		splitIds := strings.Split(ids, ",")
 
-		statuses, statusResp, ghStatusErr := ghClient.Repositories.GetCombinedStatus(c, "ministryofjustice", "cloud-platform-environments", "refs/pull/"+prNumber+"/head", &github.ListOptions{})
-		if ghStatusErr != nil {
+		for _, id := range splitIds {
+			statuses, statusResp, ghStatusErr := ghClient.Repositories.GetCombinedStatus(c, "ministryofjustice", "cloud-platform-environments", "refs/pull/"+id+"/head", &github.ListOptions{})
+			if ghStatusErr != nil {
+				obj := utils.Response{
+					Status: statusResp.StatusCode,
+					Error:  []string{ghStatusErr.Error()},
+				}
+				utils.SendResponse(c, obj)
+				return
+			}
+
+			checks, resp, ghErr := ghClient.Checks.ListCheckRunsForRef(c, "ministryofjustice", "cloud-platform-environments", "refs/pull/"+id+"/head", &github.ListCheckRunsOptions{Filter: github.String("all")})
+
+			if ghErr != nil {
+				obj := utils.Response{
+					Status: resp.StatusCode,
+					Error:  []string{ghErr.Error()},
+				}
+				utils.SendResponse(c, obj)
+				return
+			}
+
+			pendingStatusFn, prResp, ghPRErr := pull_requests.CheckPendingStatus(c, ghClient, id, time.Since)
+			if ghPRErr != nil {
+				obj := utils.Response{
+					Status: prResp.StatusCode,
+					Error:  []string{ghPRErr.Error()},
+				}
+				utils.SendResponse(c, obj)
+				return
+			}
+
+			combinedStatus := pull_requests.CheckCombinedStatus(statuses, pendingStatusFn)
+			data := pull_requests.CheckPRStatus(checks, time.Since)
+
+			data = append(data, combinedStatus...)
+
 			obj := utils.Response{
-				Status: statusResp.StatusCode,
-				Error:  []string{ghStatusErr.Error()},
+				Status: http.StatusOK,
+				Data:   data,
 			}
 			utils.SendResponse(c, obj)
 			return
 		}
-
-		checks, resp, ghErr := ghClient.Checks.ListCheckRunsForRef(c, "ministryofjustice", "cloud-platform-environments", "refs/pull/"+prNumber+"/head", &github.ListCheckRunsOptions{Filter: github.String("all")})
-
-		if ghErr != nil {
-			obj := utils.Response{
-				Status: resp.StatusCode,
-				Error:  []string{ghErr.Error()},
-			}
-			utils.SendResponse(c, obj)
-			return
-		}
-
-		pendingStatusFn, prResp, ghPRErr := pull_requests.CheckPendingStatus(c, ghClient, prNumber, time.Since)
-		if ghPRErr != nil {
-			obj := utils.Response{
-				Status: prResp.StatusCode,
-				Error:  []string{ghPRErr.Error()},
-			}
-			utils.SendResponse(c, obj)
-			return
-		}
-
-		combinedStatus := pull_requests.CheckCombinedStatus(statuses, pendingStatusFn)
-		data := pull_requests.CheckPRStatus(checks, time.Since)
-
-		data = append(data, combinedStatus...)
-
-		obj := utils.Response{
-			Status: http.StatusOK,
-			Data:   data,
-		}
-		utils.SendResponse(c, obj)
-		return
 	})
 }
--- slackbot/app.js
+++ slackbot/app.js
@@ -1,25 +1,33 @@
-const { App } = require('@slack/bolt');
+import App from '@slack/bolt';
+import fetch from 'node-fetch';
 
 // Initializes your app with your bot token and signing secret
-const app = new App({
+const app = new App.App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true, // add this
-  appToken: process.env.SLACK_APP_TOKEN, // add this
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
   port: process.env.PORT || 3000,
   logLevel: "debug"
 });
 
-// Listens to incoming messages that contain "hello"
-app.message('github', async ({ message, say }) => {
+app.message('github.com/ministryofjustice/cloud-platform-environments/pull/', async ({ message, say }) => {
   // say() sends a message to the channel where the event was triggered
   console.log('msg', message)
-  await say(`Hey there <@${message.user}>!`);
+
+  const pulls = message.text.match(/\d+/g);
+
+  const response = await fetch(`https://hammer-bot.live.cloud-platform.service.justice.gov.uk/check-pr?id=${pulls.join(",")}`);
+
+  const data = await response.json();
+
+  console.log(data);
+  // await say(`Hey there <@${message.user}>, ${message.text.split("pull/")[1]}`);
+
 });
 
 
 (async () => {
-  // Start your app
   await app.start();
 
   console.log('⚡️ Bolt app is running!');
